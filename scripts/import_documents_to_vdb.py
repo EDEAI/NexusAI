91ed8ac085264bd148af8b448ddab2b116b5569c
Ziduan Song <815331266@qq.com>
2024-12-18 18:23:30 +0800

    Modify import mechanism of import documents to vdb script

--- scripts/import_documents_to_vdb.py
+++ scripts/import_documents_to_vdb.py
@@ -1,3 +1,5 @@
+import json
+import re
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).absolute().parent.parent))
@@ -16,6 +18,7 @@ MYSQL_PASSWORD = settings.MYSQL_PASSWORD
 MYSQL_DB = settings.MYSQL_DB
 
 project_root = Path(__file__).absolute().parent.parent
+mig_dir = project_root.joinpath('docker', 'multi_service', 'vdb_migrations')
 log_dir = project_root.joinpath('logs', 'import_documents_to_vdb')
 
 documents = Documents()
@@ -39,62 +42,57 @@ def wait_for_mysql():
             time.sleep(2)
 
 def _import_document(document_name: int, user_id: int):
-    while True:
-        try:
-            documents_data = documents.get_document_find_by_name(document_name, user_id)
-            if documents_data:
-                print(documents_data)
-                dataset_data = datasets.get_dataset_by_id(documents_data['dataset_id'])
-                if dataset_data:
-                    embedding_model_config_id = dataset_data['embedding_model_config_id']
-                    models_data = models.get_model_by_config_id(embedding_model_config_id)
-                    if models_data:
-                        model_config = models_data['supplier_config']
-                        if model_config:
-                            documents.update(
-                                [
-                                    {'column': 'id', 'value': documents_data['id']},
-                                    {'column': 'status', 'op': '<', 'value': 3},
-                                ],
-                                {'status': 1}
-                            )
-                            DatasetManagement.enable_document(documents_data['id'])
-                            documents.commit()
-                            documents.close()
-                            break
-        except Exception as e:
-            documents.close()
-            print(f'Failed to enable document {documents_data["id"]}: {e}')
-        time.sleep(30)
+    documents_data = documents.get_document_find_by_name(document_name, user_id)
+    print(f'Importing document {documents_data["id"]}...')
+    assert documents_data, 'No available document!'
+    dataset_data = datasets.get_dataset_by_id(documents_data['dataset_id'])
+    assert dataset_data, 'No available dataset!'
+    embedding_model_config_id = dataset_data['embedding_model_config_id']
+    models_data = models.get_model_by_config_id(embedding_model_config_id)
+    assert models_data, 'No available embeddings model!'
+    model_config = models_data['supplier_config']
+    assert model_config, 'Please set a valid OpenAI API key!'
+    documents.update(
+        [
+            {'column': 'id', 'value': documents_data['id']},
+            {'column': 'status', 'op': '<', 'value': 3},
+        ],
+        {'status': 1}
+    )
+    DatasetManagement.enable_document(documents_data['id'])
+    documents.commit()
 
 
 def import_documents(batch_number: int):
-    match batch_number:
-        case 0:
-            document_names = ['Michael Brown-builtin-1734079638.docx',
-                            'Jane Doe-builtin-1734079639.docx', 
-                            'Emily Johnson-builtin-1734079640.docx',
-                            'John Smith-builtin-1734079641.docx',
-                            'Technical terms-builtin-1734079699.txt',
-                            'Store platform activities-builtin-1734079713.txt',
-                            'After-sales support-builtin-1734079723.txt',
-                            'Logistics consulting-builtin-1734079737.txt',
-                            'Pre-sale of goods-builtin-1734079747.txt']
+    while True:
+        try:
+            print(f'Importing batch {batch_number}...')
+            try:
+                with mig_dir.joinpath(f'{batch_number}.json').open() as f:
+                    document_names = json.load(f)
+            except:
+                raise Exception('Load JSON file error!')
             for document_name in document_names:
                 # built-in user_id is 1
                 _import_document(document_name, 1)
             print(f'Batch {batch_number} successfully imported to dataset!')
-            return
-        case _:
-            print('Invalid batch number:', batch_number)
+            break
+        except Exception as e:
+            print(f'Failed to import batch {batch_number}: {e}')
+            documents.close()
+        time.sleep(30)
 
 def run_import():
+    mig_dir.mkdir(parents=True, exist_ok=True)
     log_dir.mkdir(parents=True, exist_ok=True)
-    for batch_number in range(1):
-        log_file = log_dir.joinpath(str(batch_number))
-        if not log_file.exists():
-            import_documents(batch_number)
-            log_file.touch()
+    for mig_file in mig_dir.iterdir():
+        if mig_file.is_file() and (match := re.fullmatch(r'(\d+)\.json', mig_file.name)):
+            batch_number_str = match[1]
+            log_file = log_dir.joinpath(batch_number_str)
+            if not log_file.exists():
+                import_documents(int(batch_number_str))
+                log_file.touch()
+
 
 if __name__ == "__main__":
     wait_for_mysql()
