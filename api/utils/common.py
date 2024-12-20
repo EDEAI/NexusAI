7ab505f8ce6ab17e2ff36f849cf6a995d4dae89b
Ziduan Song <815331266@qq.com>
2024-12-20 15:32:09 +0800

    Add support for AI tools

--- api/utils/common.py
+++ api/utils/common.py
@@ -2,13 +2,19 @@ from pathlib import Path
 from typing import Any, Dict, List, Tuple, Union
 from uuid import uuid4
 from fastapi import HTTPException
+from langchain_core.messages import AIMessage
+from langchain_core.outputs import ChatGeneration
 
 import math
 
 from config import settings
 from core.database import SQLDatabase
+from core.database.models import AIToolLLMRecords, AppRuns, Models
+from core.llm import LLMPipeline, Messages
 
 project_root = Path(__file__).absolute().parent.parent.parent
+ai_tool_llm_records = AIToolLLMRecords()
+app_runs = AppRuns()
 
 def get_new_collection_name() -> str:
     """
@@ -36,6 +42,46 @@ def get_vdb_type_and_config() -> Tuple[str, Dict[str, Any]]:
             'password': settings.VDB_PASSWORD
         }
     )
+
+async def call_llm_for_ai_tool(
+    team_id: int,
+    user_id: int,
+    app_run_id: int,
+    input_messages: Messages,
+    return_json: bool = False
+) -> str:
+    model_info = Models().get_model_by_type(1, team_id, uid=user_id)
+    llm_config = {**model_info['supplier_config'], **model_info['model_config']}
+    if return_json:
+        llm_config['model_kwargs'] = {'response_format': {'type': 'json_object'}}
+    llm_pipeline = LLMPipeline(supplier=model_info['supplier_name'], config=llm_config)
+    input_ = [(role, message.value) for role, message in input_messages.messages]
+
+    # Request LLM
+    result = await llm_pipeline.llm.agenerate([input_])
+    result: ChatGeneration = result.generations[0][0]
+    output_message: AIMessage = result.message
+
+    # LLM output & token usage recording
+    if usage_metadata := output_message.usage_metadata:
+        prompt_tokens = usage_metadata['input_tokens']
+        completion_tokens = usage_metadata['output_tokens']
+        total_tokens = usage_metadata['total_tokens']
+    ai_tool_llm_records.insert(
+        {
+            'app_run_id': app_run_id,
+            'llm_input': input_,
+            'message': output_message.content,
+            'prompt_tokens': prompt_tokens,
+            'completion_tokens': completion_tokens,
+            'total_tokens': total_tokens
+        }
+    )
+    app_runs.increment_token_usage(
+        app_run_id,
+        prompt_tokens, completion_tokens, total_tokens
+    )
+    return output_message.content
 
 def response_success(data:Dict[str, Any] = {},detail:str = 'success',code:int = 0):
     """
