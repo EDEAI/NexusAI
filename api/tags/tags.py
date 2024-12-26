a634d459e9bc8c5250d6778032902e6c92d1c927
Anton4504 <1404174691@qq.com>
2024-12-26 17:03:15 +0800

    Add user_id to Tags model and implement tag management methods

--- api/tags/tags.py
+++ api/tags/tags.py
@@ -32,28 +32,15 @@ async def list_tags(mode: int, userinfo: TokenData = Depends(get_current_user)):
     - Success response with the list of tags
     """
     if mode == 0:
-        conditions = [
-            {"column": "team_id", "value": userinfo.team_id},
-            {"column": "user_id", "value": userinfo.uid},
-            {"column": "status", "value": 1},
-        ]
+        tag_data = tag.get_tag_list(user_id=userinfo.uid, team_id=userinfo.team_id)
     else:
         mode = get_tags_mode_by_app_modes(mode)
-        conditions = [
-            {"column": "team_id", "value": userinfo.team_id},
-            {"column": "user_id", "value": userinfo.uid},
-            {"column": "mode", "value": mode[0]},
-            {"column": "status", "value": 1}
-        ]
-    tag_data = tag.select(columns="*", conditions=conditions, order_by="created_time DESC")
+        tag_data = tag.get_tag_list(user_id=userinfo.uid, team_id=userinfo.team_id, mode=mode[0])
 
     # Calculate reference count for each tag
     for tag_item in tag_data:
         tag_id = tag_item['id']
-        total_count = tag_bindings.select_one(
-            aggregates={"id": "count"},
-            conditions=[{"column": "tag_id", "value": tag_id}]
-        )['count_id']
+        total_count = tag_bindings.get_tag_binding_by_count_id(tag_id=tag_id)
         tag_item['reference_count'] = total_count if total_count else 0
 
     return response_success(tag_data)
@@ -79,15 +66,8 @@ async def create_tag(request: CreateTagRequest, userinfo: TokenData = Depends(ge
         return response_error(get_language_content('team_id_not_found'))
 
     _mode = get_tags_mode_by_app_modes(request.mode)
-    tag_id = tag.insert({
-        'team_id': userinfo.team_id,
-        'user_id': userinfo.uid,
-        'mode': _mode[0],
-        'name': request.name,
-    })
-    new_tag = tag.select_one(columns={'id', 'team_id', 'user_id', 'name'}, conditions=[
-        {"column": "id", "value": tag_id}
-    ])
+    tag_id = tag.insert_tag(team_id=userinfo.team_id, user_id=userinfo.uid, mode=_mode[0], name=request.name)
+    new_tag = tag.get_tag_by_id(tag_id=tag_id, user_id=userinfo.uid, team_id=userinfo.team_id)
     return response_success(new_tag)
 
 @router.put("/tags/{tag_id}", response_model=TagResponse)
@@ -107,11 +87,10 @@ async def update_tag(tag_id: int, request: UpdateTagRequest, userinfo: TokenData
     tag_info = tag.get_tag_by_id(tag_id, userinfo.uid, userinfo.team_id)
     if not tag_info:
         return response_error(get_language_content('tag_id_not_found'))
-    tag.update(
-        [{'column': 'id', 'value': tag_id}, {'column': 'team_id', 'value': userinfo.team_id}, {'column': 'user_id', 'value': userinfo.uid}],
-        {'name': request.name}
-    )
-    return response_success(detail=get_language_content('tag_update_success'))
+    if tag.update_tag(tag_id, request.name, userinfo.team_id, userinfo.uid):
+        return response_success(detail=get_language_content('tag_update_success'))
+    else:
+        return response_error(get_language_content('tag_update_failed'))
 
 @router.delete("/tags/{tag_id}", response_model=TagResponse)
 async def delete_tag(tag_id: int, userinfo: TokenData = Depends(get_current_user)):
@@ -146,14 +125,7 @@ async def delete_tag(tag_id: int, userinfo: TokenData = Depends(get_current_user
 @router.post("/tags_bindings", response_model=TagResponse)
 async def bind_tags(request: CreateTagBindingRequest, userinfo: TokenData = Depends(get_current_user)):
     """
-    Bind multiple tags to multiple apps. An app can have multiple tags and a tag can be bound to multiple apps.
-
-    Parameters:
-    - request: CreateTagBindingRequest, containing tag_ids and app_ids
-    - userinfo: TokenData, user information for authentication
-
-    Returns:
-    - Success response with binding status
+    Bind multiple tags to multiple apps.
     """
     tag_ids = request.tag_ids
     app_ids = request.app_ids
@@ -168,43 +140,17 @@ async def bind_tags(request: CreateTagBindingRequest, userinfo: TokenData = Depe
 
     if tag_ids:
         # Check if tags exist
-        tag_data = tag.select(columns="*", conditions=[{"column": "id", "op": "in", "value": tag_ids},{"column": "status", "value": 1},{"column": "team_id", "value": userinfo.team_id},{"column": "user_id", "value": userinfo.uid}])
+        tag_data = tag.select(columns="*", conditions=[
+            {"column": "id", "op": "in", "value": tag_ids},
+            {"column": "status", "value": 1},
+            {"column": "team_id", "value": userinfo.team_id},
+            {"column": "user_id", "value": userinfo.uid}
+        ])
         if not tag_data:
             return response_error(get_language_content('tag_id_not_found'))
 
-    # Get all existing bindings for the given app_ids
-    existing_bindings = tag_bindings.select(columns=["tag_id", "app_id"], conditions=[
-        {"column": "app_id", "op": "in", "value": app_ids}
-    ])
-    existing_bindings_dict = {binding['app_id']: set() for binding in existing_bindings}
-    for binding in existing_bindings:
-        existing_bindings_dict[binding['app_id']].add(binding['tag_id'])
-
-    for app_id in app_ids:
-        existing_tag_ids = existing_bindings_dict.get(app_id, set())
-
-        if not tag_ids:
-            # Remove all bindings if tag_ids is empty
-            tag_bindings.delete([
-                {"column": "app_id", "value": app_id}
-            ])
-        else:
-            # Determine tags to add and tags to remove
-            tags_to_add = set(tag_ids) - existing_tag_ids
-            tags_to_remove = existing_tag_ids - set(tag_ids)
-
-            # Remove bindings that are no longer needed
-            if tags_to_remove:
-                tag_bindings.delete([
-                    {"column": "tag_id", "op": "in", "value": list(tags_to_remove)},
-                    {"column": "app_id", "value": app_id}
-                ])
-
-            # Add new bindings
-            for tag_id in tags_to_add:
-                tag_bindings.insert({
-                    'tag_id': tag_id,
-                    'app_id': app_id
-                })
-
-    return response_success(detail=get_language_content('tag_binding_create_success'))
+    # Use the new batch update method
+    if tag_bindings.batch_update_bindings(app_ids, tag_ids):
+        return response_success(detail=get_language_content('tag_binding_create_success'))
+    else:
+        return response_error(get_language_content('tag_binding_create_failed'))
