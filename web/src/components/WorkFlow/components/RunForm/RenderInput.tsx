7f51d05f1170e19bf2fdc1aec3edb29b11b22199
wnagchi <1305bcd@gmail.com>
2025-01-10 16:51:43 +0800

    feat：add vars create

---
+++ web/src/components/WorkFlow/components/RunForm/RenderInput.tsx
@@ -0,0 +1,42 @@
+import { ProFormDigit, ProFormTextArea } from '@ant-design/pro-components';
+import { useIntl } from '@umijs/max';
+import { Typography } from 'antd';
+import _ from 'lodash';
+
+export const RenderInput = ({ data }) => {
+    const intl = useIntl();
+    const inputs = data;
+    if (!inputs || _.isEmpty(inputs)) return null;
+    
+    return (
+        <div>
+            <Typography.Title level={5}>
+                {intl.formatMessage({
+                    id: 'workflow.title.inputParameters',
+                })}
+            </Typography.Title>
+            {Object.values(inputs).map((val: any) => {
+                if (val.type === 'number') {
+                    return (
+                        <ProFormDigit
+                            key={val.name}
+                            required={val.required}
+                            name={val.name}
+                            label={val.name}
+                        />
+                    );
+                }
+                return (
+                    <ProFormTextArea
+                        key={val.name}
+                        required={val.required}
+                        name={val.name}
+                        label={val.name}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+export default RenderInput; 
