59a4118d2a47c03bfea8e1c9b22c45ae5c69c30e
Marcus Blackburn <815331266@qq.com>
2025-04-30 17:04:28 +0800

    Fixed SQL.

--- docker/multi_service/db_migrations/202504251614.sql
+++ docker/multi_service/db_migrations/202504251614.sql
@@ -2,4 +2,4 @@ ALTER TABLE `documents`
 	ADD COLUMN `text_split_config` LONGTEXT NULL COMMENT 'Text split configuration' AFTER `node_exec_id`,
 	ADD CONSTRAINT `documents_chk_1` CHECK (json_valid(`text_split_config`));
 
-UPDATE `nexus_ai`.`dataset_process_rules` SET `config`='{"type": "RecursiveCharacterTextSplitter", "chunk_size": 4000, "chunk_overlap": 200, "keep_separator": false, "strip_whitespace": true}' WHERE  `id`=1;
+UPDATE `dataset_process_rules` SET `config`='{"type": "RecursiveCharacterTextSplitter", "chunk_size": 4000, "chunk_overlap": 200, "keep_separator": false, "strip_whitespace": true}' WHERE  `id`=1;
