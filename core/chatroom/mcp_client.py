3c8fd63407cb49121f760e02e2a5644e32417aec
Marcus Blackburn <815331266@qq.com>
2025-05-08 16:15:39 +0800

    Now MCP client supports connecting to more than 1 server.

--- core/chatroom/mcp_client.py
+++ core/chatroom/mcp_client.py
@@ -1,7 +1,4 @@
-import asyncio
-
 from contextlib import AsyncExitStack
-from datetime import datetime
 from log import Logger
 from pathlib import Path
 from typing import Any, Dict, List, Optional
@@ -18,27 +15,35 @@ logger = Logger.get_logger('chatroom')
 
 class MCPClient:
     def __init__(self):
-        self._session: Optional[ClientSession] = None
+        self._servers: Dict[str, ClientSession] = {}
+        self._tool_name_to_server_name: Dict[str, str] = {}
         self._exit_stack = AsyncExitStack()
-
-    async def connect_to_server(self):
+        
+    async def connect_to_builtin_server(self):
         client = sse_client(f'http://localhost:{settings.MCP_SERVER_PORT}/sse', timeout=10)
         sse_transport = await self._exit_stack.enter_async_context(client)
-        self._session = await self._exit_stack.enter_async_context(ClientSession(*sse_transport))
+        session = await self._exit_stack.enter_async_context(ClientSession(*sse_transport))
+        await session.initialize()
 
-        await self._session.initialize()
+        self._servers['builtin'] = session
+        response = await session.list_tools()
+        for tool in response.tools:
+            self._tool_name_to_server_name[tool.name] = 'builtin'
 
-        # List available tools
-        response = await self._session.list_tools()
-        tools = response.tools
-        logger.info(f'Successfully connected to server with tools: {[tool.name for tool in tools]}')
+    async def connect_to_server(self, server_name: str, server_config: Dict[str, Any]):
+        raise NotImplementedError('Not implemented')
 
     async def get_tool_list(self) -> List[Dict[str, Any]]:
-        response = await self._session.list_tools()
-        return [tool.model_dump() for tool in response.tools]
+        tool_list = []
+        for session in self._servers.values():
+            response = await session.list_tools()
+            for tool in response.tools:
+                tool_list.append(tool.model_dump())
+        return tool_list
 
     async def call_tool(self, tool_name: str, tool_input: Optional[Dict[str, Any]] = None) -> str:
-        response = await self._session.call_tool(tool_name, tool_input)
+        server_name = self._tool_name_to_server_name[tool_name]
+        response = await self._servers[server_name].call_tool(tool_name, tool_input)
         return response.content[0].text
 
     async def cleanup(self):
