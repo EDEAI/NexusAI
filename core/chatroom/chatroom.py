b2801e6b1a682f6f4cda70317be4eb264d58f9c0
Marcus Blackburn <815331266@qq.com>
2025-06-17 12:04:04 +0800

    Fixed a bug.

--- core/chatroom/chatroom.py
+++ core/chatroom/chatroom.py
@@ -872,7 +872,7 @@ class Chatroom:
                 if self._terminate():
                     break
 
-            self._ws_manager.end_agent_reply(self._chatroom_id)
+            await self._ws_manager.end_agent_reply(self._chatroom_id)
             self._last_speaker_id = agent_id
             app_runs.increment_steps(self._app_run_id)
             app_runs.increment_token_usage(
