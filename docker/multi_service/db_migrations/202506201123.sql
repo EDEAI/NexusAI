bfebb8c568bf587d480c41551c07cbd01ea17b1d
LHP3316 <lhp@DESKTOP-MF8S5JE.localdomain>
2025-06-20 11:29:13 +0800

    add sql field

--- docker/multi_service/db_migrations/202506201123.sql
+++ docker/multi_service/db_migrations/202506201123.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `chatrooms` ADD `last_chat_time` DATETIME NULL DEFAULT NULL COMMENT 'Last chat time' AFTER `updated_time`;
+
+ALTER TABLE `chatrooms`
+	ADD INDEX `last_chat_time` (`last_chat_time`);
