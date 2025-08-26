ace797b5a237aa26b1bf74b39f1ea3cd442cd1b7
wnagchi <1305bcd@gmail.com>
2025-08-26 17:32:16 +0800

    fix

--- web/src/components/WorkFlow/nodes/ToolNode/panel.tsx
+++ web/src/components/WorkFlow/nodes/ToolNode/panel.tsx
@@ -259,6 +259,7 @@ export default memo(({ node }: { node: AppNode }) => {
                         },
                     });
                 }
+                
             },
         });
     };
