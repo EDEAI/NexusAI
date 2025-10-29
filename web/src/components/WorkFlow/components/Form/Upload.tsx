73e83f42a9f2f0f4b65c1715a6ec77e363650236
Itachi <514749717@qq.com>
2025-10-29 14:10:27 +0800

    Optimize the display order of skill debugging and running.

--- web/src/components/WorkFlow/components/Form/Upload.tsx
+++ web/src/components/WorkFlow/components/Form/Upload.tsx
@@ -6,6 +6,7 @@ import { InboxOutlined } from '@ant-design/icons';
 import { ProFormUploadDragger, ProFormUploadDraggerProps } from '@ant-design/pro-components';
 import { useIntl } from '@umijs/max';
 import { message } from 'antd';
+import type { ReactNode } from 'react';
 
 interface UploadDraggerProps {
     name?: string;
@@ -14,6 +15,7 @@ interface UploadDraggerProps {
     multiple?: boolean;
     maxSize?: number;
     label?: string;
+    extra?: ReactNode;
 }
 
 export const UploadDragger = ({
@@ -23,12 +25,14 @@ export const UploadDragger = ({
     accept = '.txt,.md,.pdf,.html,.xlsx,.pptx,.docx,.csv,.jpg,.png,.jpeg',
     multiple = true,
     maxSize = 15,
+    extra,
 }: UploadDraggerProps) => {
     const intl = useIntl();
 
     const uploadProps: ProFormUploadDraggerProps = {
         icon: <InboxOutlined />,
         label,
+        extra,
         title: intl.formatMessage({ id: 'workflow.uploadFileText' }),
         description: intl.formatMessage({ id: 'workflow.uploadFileDes' }),
         accept,
